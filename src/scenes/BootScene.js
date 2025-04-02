f1b96877f3f287dce226a0de1e03e819bb264300
Viktor Dev <viktorbakhtin@gmail.com>
2025-04-02 19:08:59 +0300

    add build

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -7,16 +7,16 @@ export class BootScene extends Phaser.Scene {
 
     preload() {
         for (let i = gameConfig.MIN_CARD_LVL; i <= gameConfig.MAX_CARD_LVL; i++){
-            this.load.image(`card_purple${i}`, `../../assets/paintCards/${i}_purple.jpg`);
-            this.load.image(`card_red${i}`, `../../assets/paintCards/${i}_red.jpg`);
-            this.load.image(`card_yellow${i}`, `../../assets/paintCards/${i}_yellow.jpg`);
+            this.load.image(`card_purple${i}`, `/assets/paintCards/${i}_purple.jpg`);
+            this.load.image(`card_red${i}`, `/assets/paintCards/${i}_red.jpg`);
+            this.load.image(`card_yellow${i}`, `/assets/paintCards/${i}_yellow.jpg`);
         }
 
-        this.load.image('wild', '../../assets/paintCards/wild.jpg');
-        this.load.image('cardBack', '../../assets/paintCards/back.jpg');
+        this.load.image('wild', '/assets/paintCards/wild.jpg');
+        this.load.image('cardBack', '/assets/paintCards/back.jpg');
 
-        this.load.image('arrowUp', '../../assets/paintCards/arrowTop.png');
-        this.load.image('arrowDown', '../../assets/paintCards/arrowDown.png');
+        this.load.image('arrowUp', '/assets/paintCards/arrowTop.png');
+        this.load.image('arrowDown', '/assets/paintCards/arrowDown.png');
     }
 
     create() {
